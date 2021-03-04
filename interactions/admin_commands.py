2068cf09be412575401784bbb02799042f02a76a
George Popovic <georgesmpopovic@gmail.com>
2021-03-04 00:26:40 -0500

    Military format works.

--- interactions/admin_commands.py
+++ interactions/admin_commands.py
@@ -123,6 +123,35 @@ async def remind(message: Message, split_content: List[str]):
         #await ctx.send(embed=embed)
 
     else:
+        now = datetime.now()
+        date = datetime.strptime(time, '%m/%d/%Y %H:%M')
+        # %I is for 12 hour clock and %p is for AM/PM
+        day = date.strftime('%m/%d/%Y')
+        hour = date.strftime('%H:%M')
+        diff = date-now
+        seconds = diff.total_seconds()
+        print(seconds)
+
+        await message.channel.send(f"Alright, I will remind you that, and I quote, \"{reminder}\" at {hour} hours on {day}.")
+        await asyncio.sleep(seconds)
+        await user.send(f"Hi, you asked me to remind you that, and I quote, \"{reminder}\" at {hour} hours on {day}.")
+
+
+        # current_hour = str(datetime.now().hour)
+        # current_minute = str(datetime.now().minute)
+        # mt = current_hour+current_minute
+        # current_time = int(mt)
+        # desired_time = int(time)
+        # if desired_time < current_time:
+
+        #     print(current_time)
+        #     print(desired_time)
+        #     diff = abs(current_time - 24)
+        #     print(diff)
+        # else: 
+        #     diff = abs(current_time - desired_time)*60
+        #     print(diff)
+
     # # If user enters military time (24:00) rather than a specific denomination of time.
 
     #     #Get the current hour and minute from LOCAL MACHINE
@@ -131,8 +160,9 @@ async def remind(message: Message, split_content: List[str]):
 
     #     #Get the time string, break into up nto hours and minutes
     #     # e.g, 22:00 --> 10:00 PM 
-    #     hours = int(time[0:2])
-    #     minutes = int(time[2:])
+    #     split = time.split(":", 1)
+    #     hours = int(split(0))
+    #     minutes = int(split(1))
 
     #     # Special case if hours are zero, e.g., 00:15. 
     #     # or minutes, e.g., 22:00. 
